39f45ee9fd739cfb6c626a57b925c165c11e3414
Niklas Blomqvist <ntjblomqvist@gmail.com>
2024-06-08 13:40:27 +0200

    Grow snake on eating treat

--- Assets/Snake.cs
+++ Assets/Snake.cs
@@ -1,13 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
 
+    [SerializeField]
+    private GameObject tailPrefab;
+
     private float TickSpeed = 0.2f; // How often tick and move happens.
     private float MovementPerTick = 1.0f; // How much the snake moves per tick.
+
+    private Vector3 nextTailPosition;
 
     private MovementDirection CurrentDirection = MovementDirection.None;
 
@@ -28,6 +34,7 @@ public class Snake : MonoBehaviour
 
     private void Update() 
     {
+        // Handle input.
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             CurrentDirection = MovementDirection.Up;
@@ -57,15 +64,19 @@ public class Snake : MonoBehaviour
         {
             case MovementDirection.Up:
                 transform.position += Vector3.forward * MovementPerTick;
+                nextTailPosition = transform.position + Vector3.back * MovementPerTick;
                 break;
             case MovementDirection.Down:
                 transform.position += Vector3.back * MovementPerTick;
+                nextTailPosition = transform.position + Vector3.forward * MovementPerTick;
                 break;
             case MovementDirection.Left:
                 transform.position += Vector3.left * MovementPerTick;
+                nextTailPosition = transform.position + Vector3.right * MovementPerTick;
                 break;
             case MovementDirection.Right:
                 transform.position += Vector3.right * MovementPerTick;
+                nextTailPosition = transform.position + Vector3.left * MovementPerTick;
                 break;
             case MovementDirection.None:
                 break;
@@ -86,5 +97,12 @@ public class Snake : MonoBehaviour
     private void Eat(Treat treat)
     {
         treat.GetEaten();
+
+        Grow();
+    }
+
+    private void Grow()
+    {
+        Instantiate(tailPrefab, new Vector3(nextTailPosition.x, tailPrefab.transform.position.y, nextTailPosition.z), Quaternion.identity, transform);
     }
 }
