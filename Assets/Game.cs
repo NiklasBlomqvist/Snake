392a272191d38e9fcb3275814dce32ba4bb7fc78
Niklas Blomqvist <ntjblomqvist@gmail.com>
2024-06-08 13:17:03 +0200

    Only spawn on unoccupied tiles

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -13,27 +13,44 @@ public class Game : MonoBehaviour
 
     private readonly int _boardSize = 5; // [-boardSize, boardSize]
 
+    private List<Vector3> _boardPositions = new List<Vector3>(); // X and Y is the positions, Z is if it is occupied
+
     void Start()
     {
+        // Initialize the board positions
+        for (int x = 0; x < _boardSize; x++)
+        {
+            for (int z = 0; z < _boardSize; z++)
+            {
+                _boardPositions.Add(new Vector3(x + 0.5f, z + 0.5f, 0f));
+            }
+        }
+
         SpawnSnake();
         SpawnTreat();
     }
 
     private void SpawnSnake()
     {
-        var spawnPosition = GetRandomPosition();
+        var spawnPosition = GetRandomUnoccupiedPosition();
         Instantiate(snakePrefab, spawnPosition, Quaternion.identity);
     }
 
     private void SpawnTreat()
     {
-        var spawnPosition = GetRandomPosition();
+        var spawnPosition = GetRandomUnoccupiedPosition();
         Instantiate(treatPrefab, spawnPosition, Quaternion.identity);
     }
 
-    private Vector3 GetRandomPosition()
+    private Vector3 GetRandomUnoccupiedPosition()
     {
-        var randomPos = UnityEngine.Random.Range(0, _boardSize) + 0.5f;
-        return new Vector3(randomPos, 0f, randomPos);
+        // Find random unoccupied position.
+        var unoccupiedPositions = _boardPositions.FindAll(p => p.z == 0f);
+        var randomIndex = UnityEngine.Random.Range(0, unoccupiedPositions.Count);
+
+        // Occupy the position.
+        _boardPositions[_boardPositions.IndexOf(unoccupiedPositions[randomIndex])] = new Vector3(unoccupiedPositions[randomIndex].x, unoccupiedPositions[randomIndex].y, 1f);
+
+        return new Vector3(unoccupiedPositions[randomIndex].x, 0f, unoccupiedPositions[randomIndex].y);
     }
 }
