5486c262781d55bf1dc6d65456854aee126cb6ec
Niklas Blomqvist <ntjblomqvist@gmail.com>
2024-06-12 11:11:47 +0200

    Expose game settings

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -8,6 +8,13 @@ namespace SnakeGame
     public class Game : MonoBehaviour
     {
 
+        [Header("Game Settings")]
+        [SerializeField]
+        private int boardSize = 14; // Number of tiles in one axis of the board. The board is a square (_boardSize * _boardSize), starting at bottom left corner (0, 0).
+        [SerializeField]
+        private float tickSpeed = 0.15f; // How often tick and move happens.
+
+        [Header("Components")]
         [SerializeField]
         private Board board;
         [SerializeField]
@@ -15,9 +22,7 @@ namespace SnakeGame
         [SerializeField]
         private GameObject treatPrefab;
 
-        private const float TickSpeed = 0.15f; // How often tick and move happens.
         private const float MovementPerTick = 1.0f; // How much the snake moves per tick.
-        private const int BoardSize = 14; // Number of tiles in one axis of the board. The board is a square (_boardSize * _boardSize), starting at bottom left corner (0, 0).
 
         private bool _isGameOver = false;
         private Dictionary<Vector3, bool> _snakePositions;
@@ -26,7 +31,7 @@ namespace SnakeGame
 
         void Start()
         {
-            board.Initialize(BoardSize);
+            board.Initialize(boardSize);
 
             InitializeSnakePositions();
 
@@ -38,10 +43,10 @@ namespace SnakeGame
 
         private void InitializeSnakePositions()
         {
-            _snakePositions = new Dictionary<Vector3, bool>(BoardSize * BoardSize);
-            for (int x = 0; x < BoardSize; x++)
+            _snakePositions = new Dictionary<Vector3, bool>(boardSize * boardSize);
+            for (int x = 0; x < boardSize; x++)
             {
-                for (int z = 0; z < BoardSize; z++)
+                for (int z = 0; z < boardSize; z++)
                 {
                     _snakePositions.Add(new Vector3(x, 0, z), false);
                 }
@@ -52,13 +57,13 @@ namespace SnakeGame
         {
             while (!_isGameOver)
             {
-                yield return new WaitForSeconds(TickSpeed);
+                yield return new WaitForSeconds(tickSpeed);
 
                 UpdateSnakePositions();
 
                 // Check if next move will hit a wall or itself
                 var nextMove = Input.Instance.NextDirection;
-                if(nextMove == -_previousDirection) 
+                if (nextMove == -_previousDirection)
                 {
                     nextMove = _previousDirection;
                 }
@@ -88,13 +93,13 @@ namespace SnakeGame
 
         private bool IsNextMoveValid(Vector3 nextDirection)
         {
-            if(nextDirection == Vector3.zero)    
+            if (nextDirection == Vector3.zero)
                 return true;
 
             var nextPosition = _snake.GetHeadPosition() + nextDirection * MovementPerTick;
 
             // Check if next position is outside the board.
-            if (nextPosition.x < 0 || nextPosition.x >= BoardSize || nextPosition.z < 0 || nextPosition.z >= BoardSize)
+            if (nextPosition.x < 0 || nextPosition.x >= boardSize || nextPosition.z < 0 || nextPosition.z >= boardSize)
             {
                 return false;
             }
